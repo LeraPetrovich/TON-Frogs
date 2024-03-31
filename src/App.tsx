b8f81011cb542908d1f269e97bbc153a511617a7
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2024-03-31 22:43:12 +0300

    fix favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./router/AppRouter";
 import "./App.css";
+import { useTranslation } from "react-i18next";
 
 export const App: React.FC = () => {
+  const { t, i18n } = useTranslation();
+  useEffect(() => {
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute("content", t("description"));
+    }
+    document.title = t("page_title");
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
   return <AppRouter />;
 };
