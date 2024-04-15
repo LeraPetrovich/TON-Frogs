c08fe1c73797fcb5ba9f8c3905be798b11dd4d9a
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2024-04-15 19:24:56 +0300

    fix margin cat

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./slider.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
